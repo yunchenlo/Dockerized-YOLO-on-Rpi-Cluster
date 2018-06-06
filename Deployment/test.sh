59a3dbf5b4be7ebb3d67063945342eab83aca208
Jason Lo <tarzen0509@gmail.com>
2018-06-06 09:28:28 +0800

    update test.sh

---
+++ Deployment/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+helm install --name test1 \
+			--set name.request=0 --set mqtt.brokerIP="192.168.1.60", \
+			--set operator.op0.ip=192.168.1.205,operator.op0.port=8060,operator.op0.device.type=rpi,operator.op0.device.label=0 \
+			--set operator.op0.device.hostname=yclo-pi-1,operator.op0.resource.cpu=154,operator.op0.resource.mem=178         \
+			--set operator.op1.ip=192.168.1.205,operator.op1.port=8061,operator.op1.device.type=rpi,operator.op1.device.label=0         \
+			--set operator.op1.device.hostname=yclo-pi-1,operator.op1.resource.cpu=252,operator.op1.resource.mem=126         \
+			--set operator.op2.ip=192.168.1.205,operator.op2.port=8062,operator.op2.device.type=rpi,operator.op2.device.label=0         \
+			--set operator.op2.device.hostname=yclo-pi-1,operator.op2.resource.cpu=20,operator.op2.resource.mem=107  helmChart/yolo/
---
+++ README.md
@@ -0,0 +1,4 @@
+# Dockerized-YOLO-on-Rpi-Cluster
+## Developer: Yun-Chen Lo
+## Description
+> Reference Implementation of paper **"Distributed Analytics in Fog Computing Platforms Using TensorFlow and Kubernetes"**
