e62d283f4283c4c7291b12e4ac158837cbe9f33a
Jason Lo <tarzen0509@gmail.com>
2018-06-06 10:59:07 +0800

    update test

--- Deployment/test.sh
+++ Deployment/test.sh
@@ -2,7 +2,7 @@
 
 
 helm install --name test1 \
-			--set name.request=0 --set mqtt.brokerIP="192.168.1.60", \
+			--set name.request=0,name.application=1,period=6.22162374772 --set mqtt.brokerIP="192.168.1.60", \
 			--set operator.op0.ip=192.168.1.205,operator.op0.port=8060,operator.op0.device.type=rpi,operator.op0.device.label=0 \
 			--set operator.op0.device.hostname=yclo-pi-1,operator.op0.resource.cpu=154,operator.op0.resource.mem=178         \
 			--set operator.op1.ip=192.168.1.205,operator.op1.port=8061,operator.op1.device.type=rpi,operator.op1.device.label=0         \
